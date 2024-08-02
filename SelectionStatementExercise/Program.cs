ee0e5133a9a11ae4ff785818277fd179a86bfe93
milton3128 <mannmilton6@gmail.com>
2024-08-01 20:08:07 -0400

    Finished exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var r = new Random(666);
+
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Try to guess my favorite number!");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (favNumber == userInput)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else 
+            {
+                Console.WriteLine("Incorrect");
+            }
+
+            if (favNumber == userInput)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too High!");
+            }
+            else
+            {
+                Console.WriteLine("Too Low!");
+            }
+
+
         }
     }
 }
